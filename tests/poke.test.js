f42943ab805dca0802e864b181c5a627576cccf2
PragsPud <72311616+PragsPud@users.noreply.github.com>
2026-06-18 00:49:17 +0530

    Rename test suite from 'poke-simple' to 'poke-fetch'

--- tests/poke.test.js
+++ tests/poke.test.js
@@ -8,7 +8,7 @@ import {
 } from "../src/index.js";
 
 
-describe("poke-simple package", () => {
+describe("poke-fetch package", () => {
 
   test("exports all public functions", () => {
     expect(getPokemon).toBeTypeOf("function");
